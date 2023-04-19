b6ef3bb9a1c299fd0cfd2753772a363b6f2fb747
Niklas <niklas.tibbe@gmail.com>
2023-04-19 10:17:15 +0200

    links_added

--- src/app/homescreen/homescreen.component.ts
+++ src/app/homescreen/homescreen.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./homescreen.component.scss']
 })
 export class HomescreenComponent {
+  emailstring = "mailto:niklas.tibbe@outlook.de"
 
+  links = [{
+    github: 'https://github.com/mountainfex/developer_journey_pokedex',
+    linkedin: 'https://www.linkedin.com/in/niklastibbe/'
+  }];
+
+  openNewWindow(url: string){
+    window.open(url, '_blank');
+  }
 }
+
